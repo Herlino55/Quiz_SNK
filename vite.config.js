34ba2b6f960cf2cf6fe1518a36bcad62a4e4c84e
Herlin Dongmo <herlin.dongmo@gmail.com>
2025-10-17 16:05:47 +0100

    vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+// Charger le plugin uniquement en mode développement
+let vueDevTools = []
+if (process.env.NODE_ENV === 'development') {
+  const devtools = await import('vite-plugin-vue-devtools')
+  vueDevTools = [devtools.default()]
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [
     vue(),
-    // Activer les devtools uniquement en développement
-    ...(mode === 'development' ? [vueDevTools()] : []),
+    ...vueDevTools, // Devtools uniquement en local
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}))
+})
